1abe4e52440695590414ee05fbc82d3419ae0889
Vladyslav <bavdis1996@gmail.com>
2020-09-12 10:35:22 +0300

    First

---
+++ Chess.Main.cpp
@@ -0,0 +1,15 @@
+﻿#include<iostream>
+#include"Header.Chess.h"
+using namespace std;
+int main()
+{
+	setlocale(LC_CTYPE, "RUS");
+	Board game;
+	bool case_player = true;
+	while (game.menu_board(case_player))
+		case_player = !case_player;
+	if (case_player)
+		cout << "\n\n\tПОБЕДА БЕЛЫХ!!!\n\n\n";
+	cout << "\n\n\tПОБЕДА ЧЕРНЫХ!!!\n\n\n";
+	return 0;
+}
